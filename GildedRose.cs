c6cf45b5deb15c78998ac8dd9b63a8f946bd7b3e
Dzhan Halim <dzhan.halim@gmail.com>
2022-10-12 10:56:08 +0100

    Add project files.

---
+++ GildedRose.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections.Generic;
+
+namespace csharp
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+        public List<string> IncreasInQualityItems = new List<string>();
+        public GildedRose()
+        {
+            // this list is currently filled by me, but if there will be conneciton to the DB, then this code will be
+            // removed
+            IncreasInQualityItems.Add("Aged Brie");
+            IncreasInQualityItems.Add("Backstage passes to a TAFKAL80ETC concert");
+        }
+
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+            // this list is currently filled by me, but if there will be conneciton to the DB, then this code will be
+            // removed
+            IncreasInQualityItems.Add("Aged Brie");
+            IncreasInQualityItems.Add("Backstage passes to a TAFKAL80ETC concert");
+        }
+        public bool CheckIfIncreasInQualityItem(Item i)
+        {
+            if (IncreasInQualityItems.Contains(i.Name))
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        public bool CheckSpecialItem(Item i)
+        {
+            // this is a current solution, if we have more special items in the future
+            // then I would create a list of those items and check if the current item name exists in that list            
+            if (i.Name == "Sulfuras, Hand of Ragnaros")
+            {
+                return true;
+            }
+            return false;
+        }
+        public void UpdateQualityOfBackstage(Item item)
+        {
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (item.SellIn < 11)
+                {
+                    item.Quality++;
+                }
+
+                if (item.SellIn <= 6)
+                {
+                    item.Quality++;
+                }
+
+                if (item.SellIn <= 0)
+                {
+                    item.Quality = 0;
+                }
+            }
+        }
+        public void IncreaseQuality(Item i)
+        {
+            if (i.Quality < 50)
+            {
+                i.Quality++;
+            }
+            if (i.Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                UpdateQualityOfBackstage(i);
+            }
+            else
+            {
+                if (i.SellIn <= 0 && i.Quality < 50)
+                {
+                    i.Quality++;
+                }
+            }
+        }
+
+        public void DecreaseQuality(Item i)
+        {
+            if (i.Quality > 0)
+            {
+                i.Quality--;
+            }
+            if (i.SellIn <= 0 && i.Quality > 0)
+            {
+                i.Quality--;
+            }
+            // for conjured items, this is a current solution, if we have more items in the future that decrease twice
+            // each day, then I would create a list of those items and check if the current item name exists in that list            
+            if (i.Name.Contains("Conjured") && i.Quality > 0)
+            {
+                i.Quality--;
+            }
+
+        }
+        public void UpdateQuality()
+        {
+            foreach (var item in Items)
+            {
+                //if not a special item
+                if (!CheckSpecialItem(item))
+                {
+
+                    // if decrease in quality item
+                    if (!CheckIfIncreasInQualityItem(item))
+                    {
+                        DecreaseQuality(item);
+                    }
+
+                    // if increase in quality item
+                    else
+                    {
+                        IncreaseQuality(item);
+                    }
+
+                    // decrease selling day
+                    item.SellIn--;
+                    if (item.Quality > 50)
+                    {
+                        item.Quality = 50;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GildedRoseTest.cs
@@ -0,0 +1,142 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace csharp
+{
+    [TestFixture]
+    public class GildedRoseTest
+    {
+        GildedRose g = new GildedRose();
+
+        [Test]
+        public void CheckIfIncreasInQualityItemTest()
+        {
+            //Arrange
+            Item item = new Item { Name = "Aged Brie" };
+
+            //Act
+            Assert.IsTrue(g.CheckIfIncreasInQualityItem(item));
+        }
+
+        [Test]
+        public void CheckSpecialItemTest()
+        {
+            //Arrange
+            Item item = new Item { Name = "Sulfuras, Hand of Ragnaros" };
+           
+
+            //Act
+            Assert.IsTrue(g.CheckSpecialItem(item));
+
+        }
+
+        [Test]
+        public void UpdateQualityOfBackstageTest()
+        {
+            //Arrange          
+            Item backstageItem = new Item() { Name = "Backstage passes to a TAFKAL80ETC concert", Quality = 5, SellIn = -1 };
+            //Assert
+            g.UpdateQualityOfBackstage(backstageItem);
+
+            //Act
+            Assert.AreEqual(backstageItem.Quality, 0);
+
+        }
+
+        [Test]
+        public void IncreaseQualityTestNegativeSellin()
+        {
+            //Arrange            
+            Item item = new Item() { Name = "Aged Brie", Quality = 5, SellIn = -2 };
+
+            //Assert            
+            g.IncreaseQuality(item);
+
+
+            //Act
+            Assert.AreEqual(item.Quality, 7);
+
+        }
+        [Test]
+        public void IncreaseQualityTestPositiveSellin()
+        {
+            //Arrange            
+            Item item = new Item() { Name = "Aged Brie", Quality = 5, SellIn = 2 };
+
+            //Assert            
+            g.IncreaseQuality(item);
+
+
+            //Act
+            Assert.AreEqual(item.Quality, 6);
+
+        }
+        [Test]
+        public void IncreaseQualityTestMaxQuality()
+        {
+            //Arrange            
+            Item item = new Item() { Name = "Aged Brie", Quality = 50, SellIn = 2 };
+
+            //Assert            
+            g.IncreaseQuality(item);
+
+
+            //Act
+            Assert.AreEqual(item.Quality, 50);
+
+        }
+
+        [Test]
+        public void DecreaseQualityTestNegativeSellin()
+        {
+            //Arrange            
+            Item item = new Item() { Name = "Elixir of the Mongoose", Quality = 5, SellIn = -2 };
+
+            //Assert            
+            g.DecreaseQuality(item);
+
+
+            //Act
+            Assert.AreEqual(item.Quality, 3);
+
+        }
+        [Test]
+        public void DecreaseQualityTestPositiveSellin()
+        {
+            //Arrange            
+            Item item = new Item() { Name = "Elixir of the Mongoose", Quality = 5, SellIn = 2 };
+
+            //Assert            
+            g.DecreaseQuality(item);
+
+            //Act
+            Assert.AreEqual(item.Quality, 4);
+
+        }
+
+        [Test]
+        public void DecreaseQualityTestMinimumQuality()
+        {
+            //Arrange            
+            Item item = new Item() { Name = "Elixir of the Mongoose", Quality = 0, SellIn = 2 };
+
+            //Assert            
+            g.DecreaseQuality(item);
+
+            //Act
+            Assert.AreEqual(item.Quality, 0);
+        }
+        [Test]
+        public void DecreaseQualityTestConjuredItems()
+        {
+            //Arrange            
+            Item item = new Item() { Name = "Conjured Item", Quality = 1, SellIn = 2 };
+
+            //Assert            
+            g.DecreaseQuality(item);
+
+            //Act
+            Assert.AreEqual(item.Quality, 0);
+        }
+    }
+}
